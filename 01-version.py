ff2f09fdb3be0c191703cc6eb5454397cd33b50f
Fabian Garcia <garciafabianemmanuel@gmail.com>
2025-12-28 19:29:08 -0300

    version

---
+++ 01-version.py
@@ -0,0 +1,13 @@
+
+
+
+# Ejercicio 1: Mostrar la versión actual de Python instalada
+
+import sys
+
+version = ".".join(map(str, sys.version_info[:3]))
+print(f"La version actual de python es: {version}")
+
+
+
+
